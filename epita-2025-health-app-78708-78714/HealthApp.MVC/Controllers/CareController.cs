e9dd8bb450afefd8ee2d9e7920ee87508f61b13a
Clemlyon69 <clement.thorez@epita.fr>
2025-03-20 11:10:51 +0000

    Futile changes
    Created base of the Admin Panel
    Created AdminController
    Added admin user
    Added admin an care views

---
+++ epita-2025-health-app-78708-78714/HealthApp.MVC/Controllers/CareController.cs
@@ -0,0 +1,49 @@
+using HealthApp.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+
+namespace HealthApp.MVC.Controllers
+{
+    public class CareController : Controller
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<AccountController> _logger;
+
+        public CareController(SignInManager<User> signInManager,
+            ILogger<AccountController> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public IActionResult center()
+        {
+            var user = _signInManager.UserManager.GetUserAsync(User).Result;
+            if (user == null)
+            {
+                return View();
+            }
+            var userRoles = _signInManager.UserManager.GetRolesAsync(user).Result;
+            ViewBag.IsDoctor = userRoles.Contains("Doctor");
+            ViewBag.IsPatient = userRoles.Contains("Patient");
+            ViewBag.IsAdmin = userRoles.Contains("Administrator");
+
+            return View();
+        }
+
+        public IActionResult patients()
+        {
+            var user = _signInManager.UserManager.GetUserAsync(User).Result;
+            if (user == null)
+            {
+                return View();
+            }
+            var userRoles = _signInManager.UserManager.GetRolesAsync(user).Result;
+            ViewBag.IsDoctor = userRoles.Contains("Doctor");
+            ViewBag.IsPatient = userRoles.Contains("Patient");
+            ViewBag.IsAdmin = userRoles.Contains("Administrator");
+
+            return View();
+        }
+    }
+}
